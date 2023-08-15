320a077941f684342e824549e9865d362042ae24
Riddy105 <olaniranolatubosun@gmail.com>
2023-08-15 14:58:39 +0100

    ci: implemented checkout feature

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -4,17 +4,29 @@ interface CustomInputProps {
   label: string;
   errorText: string;
   name: string;
+  onChange: any;
+  onBlur: any;
+  value: string;
+  hasError: boolean;
 }
 const CustomInput = (props: CustomInputProps) => {
-  const { label, errorText, name } = props;
+  const { label, errorText, name, onBlur, onChange, value, hasError } = props;
   return (
     <IonInput
+      className={!!hasError ? "ion-invalid ion-touched" : ""}
       name={name}
       label={label}
+      value={value}
       labelPlacement="floating"
       errorText={errorText}
+      onIonChange={onChange}
+      onIonBlur={onBlur}
     ></IonInput>
   );
 };
 
 export default CustomInput;
+
+// I faced a challenge using the IonInput component, initially I tried to compute the classname from the errorText prop. errorText was either the error message I wanted to show in a case where there is an error, otherwise it'd be an empty string.
+// Passing an empty string as a value to the errorText prop on IonInput gave rise to an unusual behaviour where it took the styling off the element. I then had to settle for always passing an error message to the errorText so that it is always a string with characters.
+// I then used the "hasError" prop which is always a boolean to dynamically compute the classname. If true, "ion-invalid ion-touched" is injected into the classname as these two classes are required to bring the errorText on the screen.
--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -4,15 +4,20 @@ interface CustomSelectProps {
   options: string[] | number[];
   label: string;
   name: string;
+  onChange: any;
+  onBlur: any;
+  hasError: boolean;
 }
 const CustomSelect = (props: CustomSelectProps) => {
-  const { options, label, name } = props;
+  const { options, label, name, onChange, onBlur, hasError } = props;
   return (
     <IonSelect
       name={name}
       label={label}
       labelPlacement="floating"
-      className="border-b border-[#ddd]"
+      className={`border-b ${hasError ? "border-[#f00]" : "border-[#ddd]"}`}
+      onIonChange={onChange}
+      onIonBlur={onBlur}
     >
       {options.map((opt, index) => (
         <IonSelectOption value={opt} key={index}>
